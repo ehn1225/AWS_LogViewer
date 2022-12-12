ba8098d2a413ee22f633e1017956c1bd558c7832
LeeYeChan <ehn1225@naver.com>
2022-12-12 13:00:16 +0900

    [AWS_LogViewer][이예찬] userIdentity 필터 기능 추가

--- _include/script.js
+++ _include/script.js
@@ -17,26 +17,31 @@ window.onload = function(){
 		}
 	};
 }
-function Readfiles(){
+function Readfiles(filter = 'non'){
 	//https://docs.aws.amazon.com/ko_kr/awscloudtrail/latest/userguide/cloudtrail-event-reference-user-identity.html
 	let tbody1 = document.getElementById('tbody');
 	let log_count = document.getElementById('log_count');
 	let eventName_count = document.getElementById('eventName_count');
-	let count = 1;
+	let count = 0;
 	var user_count = new Map();
 	var event_count = new Map();
 	tbody1.innerHTML = "";
 	log_count.innerHTML = "";
 	eventName_count.innerHTML = "";
-	
 	for(let file of files){
 		for(let obj of file){
+			count++;
 			let innerHtml = "";
 
 			var type = obj.userIdentity.type;
 			if(type == "IAMUser"){
 				type = obj.userIdentity.userName;
 			}
+
+			if(filter != 'non' && type != filter){
+				continue;
+			}
+
 			innerHtml = "<tr class=\"view\"><td>" + obj.eventTime + "</td><td>" + type +"</td><td>" + obj.eventName + "</td><td>" + obj.eventType + "</td><td>" + obj.sourceIPAddress + "</td><td>"+ obj.userAgent +"</td><td width=\"50\">" + file.fileName + "</td></tr><tr class=\"fold\"><td colspan=\"7\"><div class=\"fold-content\">" + JSON.stringify(obj) + "</div></td></tr>";
 			tbody1.innerHTML += innerHtml;
 			
@@ -60,11 +65,15 @@ function Readfiles(){
 		}
 	}
 	for (let entry of user_count) {
-		log_count.innerHTML += "<li class=\"list-group-item d-flex justify-content-between align-items-center\">" + entry[0] + "<span class=\"badge bg-primary rounded-pill\">" + entry[1] + "</span></li>";	
+		log_count.innerHTML += "<li class=\"list-group-item d-flex justify-content-between align-items-center\" onclick=\"Readfiles('" + entry[0] + "')\">" + entry[0] + "<span class=\"badge bg-primary rounded-pill\">" + entry[1] + "</span></li>";	
   	}
 	for (let entry of event_count) {
 		eventName_count.innerHTML += "<li class=\"list-group-item d-flex justify-content-between align-items-center\">" + entry[0] + "<span class=\"badge bg-primary rounded-pill\">" + entry[1] + "</span></li>";	
   	}
+	if(filter != 'non'){
+		log_count.innerHTML += "<li class=\"list-group-item d-flex justify-content-between align-items-center\" onclick=\"Readfiles()\">필터 적용 취소<span class=\"badge bg-primary rounded-pill\">" + count + "</span></li>";
+	}
+
 
 	$(function () {
 		$(".fold-table tr.view").on("click", function () {
